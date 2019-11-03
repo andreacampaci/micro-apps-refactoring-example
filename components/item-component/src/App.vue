0989488f27631e245140ee86e10089f87cb02ffa
Andrea Campaci <andreacampaci@hotmail.it>
2019-11-03 10:57:25 +0100

    integration item into portal phase 2

--- components/item-component/src/App.vue
+++ components/item-component/src/App.vue
@@ -9,9 +9,9 @@
                     </div>
                     <div>
                         <h3>
-                            <b-badge variant="primary">{{item.price}}</b-badge>
+                            <b-badge variant="primary">${{item.price}}</b-badge>
                         </h3>
-                        <b-button variant="success" @click="addCart()">Add to Cart</b-button>
+                        <b-button variant="success" @click="addCart($event, item)">Add to Cart</b-button>
                     </div>
                 </div>
             </b-card>
@@ -26,17 +26,26 @@
 </template>
 
 <script lang="ts">
-    import {Component, Vue, Prop, Watch} from 'vue-property-decorator';
-    import {
-        BPagination,
-    } from 'bootstrap-vue';
+    import axios from 'axios';
+    import VueAxios from 'vue-axios';
+    import BootstrapVue from 'bootstrap-vue';
 
-    @Component({
-        components: {
-            'b-pagination': BPagination,
-        },
-    })
+    import 'bootstrap/dist/css/bootstrap.css';
+    import 'bootstrap-vue/dist/bootstrap-vue.css';
+    import { BPagination } from 'bootstrap-vue';
+    Vue.use(BootstrapVue);
+    Vue.component('b-pagination', BPagination);
+    Vue.use(VueAxios, axios);
+    import {Component, Vue, Prop, Watch} from 'vue-property-decorator';
+
+    @Component
     export default class App extends Vue {
+
+        @Prop({
+            default: () => {
+                return "";
+            }
+        }) private category!: string;
         public currentPage = 1;
         public perPage = 3;
         private items: any[] = [];
@@ -49,17 +58,28 @@
         }
 
         get itemsPaginated() {
-            return this.items.slice((this.currentPage - 1) * this.perPage, (this.currentPage) * this.perPage);
+            if (this.category !== "") {
+                return this.items
+                    .filter(item => item.category === this.category)
+                    .slice((this.currentPage - 1) * this.perPage, (this.currentPage) * this.perPage);
+            }
+            return this.items
+                .slice((this.currentPage - 1) * this.perPage, (this.currentPage) * this.perPage);
         }
 
         get rows() {
+            if (this.category !== "") {
+                return this.items
+                    .filter(item => item.category === this.category)
+                    .length;
+            }
             return this.items.length;
         }
 
-        public addCart(event: any) {
-            event.target.dispatchEvent(new CustomEvent('my-click', {
+        public addCart($event: any, item: any) {
+            $event.target.dispatchEvent(new CustomEvent('addcart', {
                 bubbles: true,
-                detail: 'clicked',
+                detail: item,
                 composed: true,
             }));
         }
@@ -67,6 +87,13 @@
 </script>
 
 <style lang="scss" scoped>
+    @import 'node_modules/bootstrap/dist/css/bootstrap';
+    @import 'node_modules/bootstrap-vue/dist/bootstrap-vue';
+    .VueWrapper {
+        border: 2px dashed #42b983;
+        padding: 0.5em;
+        font-size: 1.5em;
+    }
     .List {
         &__item {
             margin-bottom: 1em;
--- portal/phase-2/js/productListCtrl.js
+++ portal/phase-2/js/productListCtrl.js
@@ -1,13 +1,31 @@
 angular.module("sportsStore")
     .constant("productListActiveClass", "btn-primary")
+    .constant("components", [])
     .constant("productListPageCount", 3)
-    .controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount, cart) {
+    .controller("productListCtrl", function ($scope, $filter, productListActiveClass, components, productListPageCount, cart) {
         var selectedCategory = null;
         $scope.selectedPage = 1;
         $scope.pageSize = productListPageCount;
+        $scope.category = null;
+
+        $scope.init = function () {
+            console.log(components);
+            console.log('preparing to load...');
+            const node = document.createElement('script');
+            node.src = 'http://127.0.0.1:3001/item-component.js';
+            if (components.filter(cmp => cmp === node.src).length === 0) {
+                components.push(node.src);
+                node.type = 'text/javascript';
+                node.async = false;
+                document.getElementsByTagName('head')[0].appendChild(node);
+            }
+        };
+
+        $scope.init();
 
         $scope.selectCategory = function (newCategory) {
             selectedCategory = newCategory;
+            $scope.category = selectedCategory;
             $scope.selectedPage = 1;
         }
 
@@ -24,7 +42,9 @@ angular.module("sportsStore")
         $scope.getPageClass = function (page) {
             return $scope.selectedPage == page ? productListActiveClass : "";
         }
-        $scope.addProductToCart = function (product) {
+
+        $scope.addProductToCart = function ($event) {
+            var product = $event.detail;
             cart.addProduct(product.id, product.name, product.price);
         }
     });
